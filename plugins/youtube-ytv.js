f4757bd1ae704e6e900a597b83fd2bc31b88edef
THE-27 <blackoficial2025@gmail.com>
2025-06-10 08:34:33 -0500

    fix

--- plugins/youtube-ytv.js
+++ plugins/youtube-ytv.js
@@ -1,111 +1,32 @@
 import fetch from 'node-fetch';
-import yts from 'yt-search';
-import { youtubedl, youtubedlv2 } from '@bochilteam/scraper';
 
-let limit = 100; 
+let handler = async(m, { conn, usedPrefix, command, text }) => {
 
-let handler = async (m, { conn: star, args, usedPrefix, command }) => {
-  if (!args || !args[0]) {
-    return star.reply(
-      m.chat,
-      `✦ *¡Ingresa el texto o enlace del vídeo de YouTube!*\n\n» *Ejemplo:*\n> *${usedPrefix + command}* Canción de ejemplo`,
-      m
-    );
+if (!text) return m.reply(`🍭 Ingresa Un Texto Para Buscar En Youtube\n> *Ejemplo:* ${usedPrefix + command}crow edits`);
+
+try {
+let api = await (await fetch(`https://delirius-apiofc.vercel.app/search/ytsearch?q=${text}`)).json();
+
+let results = api.data[0];
+
+let txt = `✨ *Título:* ${results.title}\n⌛ *Duración:* ${results.duration}\n📎 *Link:* ${results.url}\n📆 *Publicado:* ${results.publishedAt}`;
+
+let img = results.image;
+
+conn.sendMessage(m.chat, { image: { url: img }, caption: txt }, { quoted: m });
+
+let api2 = await(await fetch(`https://api.fgmods.xyz/api/downloader/ytmp3?url=${results.url}&quality=480p&apikey=elrebelde21`)).json();
+
+if (!api2?.result.dl_url) return m.reply('No Se  Encontraron Resultados');
+
+await conn.sendMessage(m.chat, { document: { url: api2.result.dl_url }, mimetype: 'audio/mpeg', fileName: `${api2.result.title}.mp3` }, { quoted: m });
+
+} catch (e) {
+m.reply(`Error: ${e.message}`);
+m.react('✖️');
   }
+}
 
-  await m.react('🕓'); 
-
-  try {
-    let query = args.join(' ');
-    let isUrl = query.match(/youtu/gi);
-
-    let video;
-    if (isUrl) {
-
-      let ytres = await yts({ videoId: query.split('v=')[1] });
-      video = ytres.videos[0];
-    } else {
-      // Si es un texto
-      let ytres = await yts(query);
-      video = ytres.videos[0];
-      if (!video) {
-        return star.reply(m.chat, '✦ *Video no encontrado.*', m).then(() => m.react('✖️'));
-      }
-    }
-
-    let { title, thumbnail, timestamp, views, ago, url } = video;
-
-    let yt = await youtubedl(url).catch(async () => await youtubedlv2(url));
-    let videoInfo = yt.video['360p']; 
-
-    if (!videoInfo) {
-      return star.reply(m.chat, '✦ *No se encontró una calidad compatible para el video.*', m).then(() => m.react('✖️'));
-    }
-
-    let { fileSizeH: sizeHumanReadable, fileSize } = videoInfo;
-
-
-    let sizeMB = fileSize / (1024 * 1024); 
-
-
-    if (sizeMB >= 700) {
-      return star.reply(m.chat, '✦ *El archivo es demasiado pesado (más de 700 MB). Se canceló la descarga.*', m).then(() => m.react('✖️'));
-    }
-
-
-    let durationInMinutes = parseFloat(timestamp.split(':')[0]) * 60 + parseFloat(timestamp.split(':')[1]);
-
-
-    let txt = `✦ *Título:* » ${title}\n`;
-    txt += `✦ *Duración:* » ${timestamp}\n`;
-    txt += `✦ *Visitas:* » ${views}\n`;
-    txt += `✦ *Subido:* » ${ago}\n`;
-    txt += `✦ *Tamaño:* » ${sizeHumanReadable}\n\n`;
-    //txt += `> *- ↻ El video se está enviando, espera un momento...*`;
-
-
-    await star.sendFile(m.chat, thumbnail, 'thumbnail.jpg', txt, m);
-
-
-    let api = await fetch(`https://api.siputzx.my.id/api/d/ytmp4?url=${url}`);
-    let json = await api.json();
-    let { data } = json;
-
-    if (!data || !data.dl) {
-      return star.reply(m.chat, '✦ *Error al obtener el enlace de descarga desde la API.*', m).then(() => m.react('✖️'));
-    }
-
-    let { dl: downloadUrl } = data;
-
-    // Enviar el video según el tamaño o la duración
-    if (sizeMB > limit || durationInMinutes > 30) {
-      // Enviar como documento si el tamaño supera los 100 MB o si dura más de 30 minutos
-      await star.sendMessage(
-        m.chat,
-        { document: { url: downloadUrl }, mimetype: 'video/mp4', fileName: `${title}.mp4` },
-        { quoted: m }
-      );
-      await m.react('📄'); // Reacción de documento
-    } else {
-      // Enviar como video normal si es menor o igual al límite y dura menos de 30 minutos
-      await star.sendMessage(
-        m.chat,
-        { video: { url: downloadUrl }, caption: `${title}`, mimetype: 'video/mp4', fileName: `${title}.mp4` },
-        { quoted: m }
-      );
-      await m.react('✅'); // Reacción de éxito
-    }
-  } catch (error) {
-    console.error(error);
-    await m.react('✖️'); // Error durante el proceso
-    star.reply(m.chat, '✦ *Ocurrió un error al procesar tu solicitud. Intenta nuevamente más tarde.*', m);
-  }
-};
-
-
-handler.help = ['ytv'];
-handler.tags = ['descargas'];
 handler.command = ['ytv'];
-handler.coin = 4;
 
-export default handler;
+export default handler
