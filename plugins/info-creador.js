881a6ee7d554c59ac9c91c146db7e22f31ec0c8a
THE-27 <blackoficial2025@gmail.com>
2025-06-10 21:01:09 -0500

    Update

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -1,62 +1,35 @@
-/*
-function handler(m) {
-let name = conn.getName(`${suittag}@s.whatsapp.net`)
-let ownerN = `${suittag}`
-conn.sendContact(m.chat, [[`${ownerN}@s.whatsapp.net`, `${name}`]], m, {
- contextInfo: { 
- forwardingScore: 2023,
-isForwarded: false, 
- externalAdReply: {  
- title: packname, 
- body: dev, 
- sourceUrl: channel,
- thumbnail: banner,
- thumbnailUrl: banner, 
- mediaType: 1,
- showAdAttribution: true, 
- renderLargerThumbnail: true 
- }
-   }
-     },
-       {
-         quoted: m
-           }
-             );
-
-}
-
-handler.help = ['owner']
-handler.tags = ['main']
-handler.command = ['owner', 'creator', 'creador', 'dueño'] 
-
-export default handler
-*/
-
-
-
-
 function handler(m, { conn}) {
   const suittag = '51969214380'; 
-  const packname = 'hola';
-  const dev = 'hola xd';
+  const name = 'BLACK.OFC';
+  const packname = '✦⃟⛧┋ ➪ _R I N ⛧ I T O S H I_ ⚽┋⃟✧';
+  const dev = '© Modified by:  ꧁𓊈𒆜𝖙𝖍𝖊•𝒃𝒍𝒂𝒄𝒌𒆜𓊉꧂';
   const channel = 'https://whatsapp.com/channel/0029Vb6BDQc0lwgsDN1GJ31i';
   const banner = 'https://files.catbox.moe/pp7ncd.jpg';
+  
+  const vcard = `BEGIN:VCARD
+VERSION:3.0
+N:;${name};;;
+FN:${name}
+ORG:PANDA
+TEL;type=CELL;type=VOICE;waid=${suittag}:${suittag}
+END:VCARD`;
 
-  let name = conn.getName(`${suittag}@s.whatsapp.net`);
-  let ownerN = `${suittag}`;
-
-  conn.sendContact(m.chat, [[`${ownerN}@s.whatsapp.net`, `${name}`]], m, {
+  conn.sendMessage(m.chat, {
+    contacts: {
+      displayName: `${name}`,
+      contacts: [{ vcard}]
+  },
     contextInfo: {
-      forwardingScore: 2023,
-      isForwarded: false,
+      forwardingScore: 999,
+      isForwarded: true,
       externalAdReply: {
+        showAdAttribution: true,
         title: packname,
         body: dev,
-        sourceUrl: channel,
-        thumbnail: banner,
         thumbnailUrl: banner,
         mediaType: 1,
-        showAdAttribution: true,
+        mediaUrl: channel,
+        sourceUrl: channel,
         renderLargerThumbnail: true
       }
     }
