71b613d414482506a18372017d7cc9f379d8318b
THE-27 <blackoficial2025@gmail.com>
2025-06-10 20:30:53 -0500

    Update

--- plugins/grupo-tagall.js
+++ plugins/grupo-tagall.js
@@ -1,3 +1,4 @@
+/*
 const handler = async (m, { isOwner, isAdmin, conn, text, participants, args, command, usedPrefix }) => {
   if (usedPrefix == 'a' || usedPrefix == 'A') return;
 
@@ -22,7 +23,7 @@ const handler = async (m, { isOwner, isAdmin, conn, text, participants, args, co
   }
   teks += `╰══⌬\n> ${dev}`;
 
-  conn.sendMessage(m.chat, { text: teks, mentions: participants.map((a) => a.id) });
+  conn.sendMessage(m.chat, { text: teks, mentions: participants.map((a) => a.id) fkontak});
 };
 
 handler.help = ['todos *<mensaje opcional>*'];
@@ -31,5 +32,48 @@ handler.command = ['todos', 'invocar', 'tagall', 'marcar']
 handler.admin = true;
 handler.group = true;
 handler.coin = 4; 
+
+export default handler;
+
+*/
+
+const handler = async (m, { isOwner, isAdmin, conn, text, participants, args, command, usedPrefix}) => {
+  if (usedPrefix === 'a' || usedPrefix === 'A') return;
+
+  const customEmoji = global.db.data.chats[m.chat]?.customEmoji || '🔥';
+  await m.react(customEmoji);
+
+  if (!(isAdmin || isOwner)) {
+    global.dfail('admin', m, conn);
+    return;
+}
+
+  const pesan = args.join(' ');
+  const oi = `*⚽ » INFO:* ${pesan}`;
+
+  let teks = `╭━━⬣\n`;
+  teks += `╭┤👑 *! MENCION GENERAL!*\n`;
+  teks += `┃│🍬 *PARA ${participants.length} MIEMBROS*\n`;
+  teks += `┃│ ${oi}\n`;
+  teks += `┃╰━━⬣\n╰════════⬣\n`;
+
+  for (const mem of participants) {
+    teks += `┃${customEmoji} @${mem.id.split('@')[0]}\n`;
+}
+
+  teks += `╰══⌬\n> 🔹 RIN BOT`;
+
+  await conn.sendMessage(m.chat, {
+    text: teks,
+    mentions: participants.map(a => a.id)
+}, { quoted: m});
+};
+
+handler.help = ['todos *<mensaje opcional>*'];
+handler.tags = ['group'];
+handler.command = ['todos', 'invocar', 'tagall', 'marcar'];
+handler.admin = true;
+handler.group = true;
+handler.coin = 4;
 
 export default handler;
