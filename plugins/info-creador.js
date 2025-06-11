3766b153b530d1efea170055a9d282c70fe4f608
THE-27 <blackoficial2025@gmail.com>
2025-06-10 20:48:12 -0500

    Update

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -1,63 +1,67 @@
-/*import fetch from 'node-fetch';
+/*
+function handler(m) {
+let name = conn.getName(`${suittag}@s.whatsapp.net`)
+let ownerN = `${suittag}`
+conn.sendContact(m.chat, [[`${ownerN}@s.whatsapp.net`, `${name}`]], m, {
+ contextInfo: { 
+ forwardingScore: 2023,
+isForwarded: false, 
+ externalAdReply: {  
+ title: packname, 
+ body: dev, 
+ sourceUrl: channel,
+ thumbnail: banner,
+ thumbnailUrl: banner, 
+ mediaType: 1,
+ showAdAttribution: true, 
+ renderLargerThumbnail: true 
+ }
+   }
+     },
+       {
+         quoted: m
+           }
+             );
 
-let handler = async (m, { conn, usedPrefix, text, args, command }) => {
-   await m.react('☕');
+}
 
-    let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender;
-    let name = await conn.getName(who);
-    let edtr = `@${m.sender.split`@`[0]}`;
-    let username = conn.getName(m.sender);
+handler.help = ['owner']
+handler.tags = ['main']
+handler.command = ['owner', 'creator', 'creador', 'dueño'] 
 
-    // VCARD
-    let list = [{
-        displayName: "the_Black•OFC",
-        vcard: `BEGIN:VCARD\nVERSION:3.0\nFN: the_Black•OFC
-\nitem1.TEL;waid=51969214380:51969214380\nitem1.X-ABLabel:Número\nitem2.EMAIL;type=INTERNET: blackoficial2025@gmail.com\nitem2.X-ABLabel:Email\nitem3.URL:https://whatsapp.com/channel/0029Vb6BDQc0lwgsDN1GJ31i\nitem3.X-ABLabel:Internet\nitem4.ADR:;; Perú;;;;\nitem4.X-ABLabel:Region\nEND:VCARD`,
-    }];
-
-    await conn.sendMessage(m.chat, {
-        contacts: {
-            displayName: `${list.length} Contacto`,
-            contacts: list
-        },
-        contextInfo: {
-            externalAdReply: {
-                showAdAttribution: true,
-                title: 'һ᥆ᥣᥲ s᥆ᥡ ᥱᥣ ᥴrᥱᥲძ᥆r ძᥱ rіn ᑲ᥆𝗍',
-                body: dev,
-                thumbnailUrl: 'https://files.catbox.moe/so2ocj.jpg',
-                sourceUrl: 'https://wa.me/51969214380?text=Vengo+Del+Comando+.owner',
-                mediaType: 1,
-                renderLargerThumbnail: true
-            }
-        }
-    }, {
-        quoted: m
-    });
-
-    let txt = ``;
-
-    await conn.sendMessage(m.chat, { text: txt });
-};
-
-handler.help = ['owner', 'creador'];
-handler.tags = ['info'];
-handler.command = ['owner', 'creator', 'creador', 'dueño'];
-
-export default handler;
+export default handler
 */
 
 
-let handler = async (m, { conn}) => {
-  let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;BRAYAN-GT;;\nFN:BRAYAN-GT\nORG:PANDA\nTITLE:\nitem1.TEL;waid=593979148034:+593979148034\nitem1.X-ABLabel:BRAYAN-GT\nX-WA-BIZ-DESCRIPTION:\nX-WA-BIZ-NAME:PANDA🐼\nEND:VCARD`;
 
-  await conn.sendMessage(m.chat, {
-    contacts: {
-      displayName: 'おBrayan.xyz',
-      contacts: [{ vcard}]
+
+function handler(m, { conn}) {
+  const suittag = suittag; 
+  const packname = packname;
+  const dev = dev;
+  const channel = 'https://whatsapp.com/channel/0029Vb6BDQc0lwgsDN1GJ31i';
+  const banner = 'https://files.catbox.moe/pp7ncd.jpg';
+
+  let name = conn.getName(`${suittag}@s.whatsapp.net`);
+  let ownerN = `${suittag}`;
+
+  conn.sendContact(m.chat, [[`${ownerN}@s.whatsapp.net`, `${name}`]], m, {
+    contextInfo: {
+      forwardingScore: 2023,
+      isForwarded: false,
+      externalAdReply: {
+        title: packname,
+        body: dev,
+        sourceUrl: channel,
+        thumbnail: banner,
+        thumbnailUrl: banner,
+        mediaType: 1,
+        showAdAttribution: true,
+        renderLargerThumbnail: true
+      }
     }
   }, { quoted: m});
-};
+}
 
 handler.help = ['owner'];
 handler.tags = ['main'];
