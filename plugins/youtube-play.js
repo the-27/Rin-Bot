85a3695daceb5a6aa40c60369f0b1509086c954e
THE-27 <blackoficial2025@gmail.com>
2025-06-11 13:18:30 -0500

    「 ⚽ 𝐑𝐈𝐍 - 𝐈𝐓𝐎𝐒𝐇𝐈 🌴 」

--- plugins/youtube-play.js
+++ plugins/youtube-play.js
@@ -1,47 +1,52 @@
 import yts from 'yt-search';
 
-const handler = async (m, { conn, text, usedPrefix, command }) => {
+const handler = async (m, { conn, text, usedPrefix, command}) => {
   if (!text) {
-    throw `❗ Por favor ingresa un texto para buscar.\nEjemplo: ${usedPrefix + command} Nombre del video`;
+    return conn.reply(m.chat, `❗ Por favor ingresa un texto para buscar.\nEjemplo: ${usedPrefix + command} Nombre del video`, m);
   }
 
-  
-  const search = await yts(text);
-  const videoInfo = search.all?.[0];
+  try {
+    const search = await yts(text);
+    const videoInfo = search.all?.[0];
 
-  if (!videoInfo) {
-    throw '❗ No se encontraron resultados para tu búsqueda. Intenta con otro título.';
+    if (!videoInfo) {
+      return conn.reply(m.chat, '❗ No se encontraron resultados para tu búsqueda. Intenta con otro título.', m);
   }
 
-  const body = `
-> 𑁯᧙  🍓 *Título:* ${video.title}
-> 𑁯᧙  📏 *Duración:* ${video.timestamp}
-> 𑁯᧙  👁️ *Vistas:*  ${video.views.toLocaleString()}
-> 𑁯᧙  🎨 *Autor:* ${video.author.name}
-> 𑁯᧙  🕰️ *Publicado:* ${video.ago}
-> 𑁯᧙  📝 *vídeo url:* ${video.url}`;
+    const body = `
+> 𑁯᧙ 🍓 *Título:* ${videoInfo.title}
+> 𑁯᧙ 📏 *Duración:* ${videoInfo.timestamp}
+> 𑁯᧙ 👁️ *Vistas:* ${videoInfo.views.toLocaleString()}
+> 𑁯᧙ 🎨 *Autor:* ${videoInfo.author.name}
+> 𑁯᧙ 🕰️ *Publicado:* ${videoInfo.ago}
+> 𑁯᧙ 📝 *Vídeo URL:* ${videoInfo.url}`;
 
-  await conn.sendMessage(
-    m.chat,
-    {
-      image: { url: videoInfo.thumbnail },
-      caption: body,
-      footer: `Elige una de las opciones para descargar:`,
-      buttons: [
-        { buttonId: `.yta ${videoInfo.url}`, buttonText: { displayText: '🎵 Audio' } },
-        { buttonId: `.ytv ${videoInfo.url}`, buttonText: { displayText: '📽️ Video' } },
-      ],
-      viewOnce: true,
-      headerType: 4,
-    },
-    { quoted: m }
-  );
-  m.react('✅'); // Reacción de éxito
+    await conn.sendMessage(
+      m.chat,
+      {
+        image: { url: videoInfo.thumbnail},
+        caption: body,
+        footer: 'Elige una opción para descargar:',
+        buttons: [
+          { buttonId: `${usedPrefix}yta ${videoInfo.url}`, buttonText: { displayText: '🎵 Audio'}, type: 1},
+          { buttonId: `${usedPrefix}ytv ${videoInfo.url}`, buttonText: { displayText: '📽️ Video'}, type: 1},
+        ],
+        viewOnce: true,
+        headerType: 4,
+      },
+      { quoted: m}
+    );
+
+    await m.react('✅'); // Reacción de éxito
+  } catch (error) {
+    console.error(error);
+    return conn.reply(m.chat, `❗ Ocurrió un error: ${error.message}`, m);
+ }
 };
 
 handler.command = ['play3'];
-handler.tags = ['descargas']
-handler.group = true
-handler.limit = 6
+handler.tags = ['descargas'];
+//handler.group = true;
+handler.limit = 6;
 
 export default handler;
