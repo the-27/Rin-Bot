6bddd88c5ae43ec35d6c4a07f0a4aecd3260c215
THE-27 <blackoficial2025@gmail.com>
2025-06-11 19:21:20 -0500

    「 ⚽ 𝐑𝐈𝐍 - 𝐈𝐓𝐎𝐒𝐇𝐈 🌴 」

---
+++ plugins/main-menu•dl.js
@@ -0,0 +1,41 @@
+let handler = async (m, { conn, args }) => {
+    let userId = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.sender
+    let user = global.db.data.users[userId]
+    let name = conn.getName(userId)
+    let txt = `
+Hola @${userId.split('@')[0]} aqui esta el menu de descargas
+ play 
+ apk
+ apk2
+`.trim()
+
+  await conn.sendMessage(m.chat, { 
+      text: txt,
+      contextInfo: {
+          mentionedJid: [m.sender, userId],
+          isForwarded: true,
+          forwardedNewsletterMessageInfo: {
+              newsletterJid: channelRD.id,
+              newsletterName: channelRD.name,
+              serverMessageId: -1,
+          },
+          forwardingScore: 999,
+          externalAdReply: {
+              title: botname,
+              body: textbot,
+              thumbnailUrl: banner,
+              sourceUrl: redes,
+              mediaType: 1,
+              showAdAttribution: true,
+              renderLargerThumbnail: true,
+          },
+      },
+  }, { quoted: m })
+
+}
+
+handler.help = ['menudl']
+handler.tags = ['main']
+handler.command = ['menudescargas', 'menudl']
+
+export default handler
